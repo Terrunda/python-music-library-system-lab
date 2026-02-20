b5ebf5fff6ff522e6d63e03339c263e1d7168ec1
Terrunda <terrencegift49@gmail.com>
2026-02-20 23:10:57 +0300

    Reformatted sections of the code to ensure that the tests are succesful.

--- lib/song.py
+++ lib/song.py
@@ -29,18 +29,16 @@ class Song:
     
     @classmethod
     def add_to_genre_count(cls, genre): #if genre doesnt exist in genre_count, add the key and set it to 1.
-        if cls.genre_count[genre] not in cls.genre_count:
-            cls.genre_count.update({str(genre): 1 })
-        else:
-            cls.genre_count[genre] +=1
+        cls.genre_count[genre] = cls.genre_count.get(genre, 0) + 1
+        return cls.genre_count
     
     @classmethod
-    def add_to_artists_count(cls, artist):
-        if cls.artist_count[artist] not in cls.artist_count: #if artist doesn't exist in artist_count, add the artist as the key and set it to 1.
-            cls.artist_count.update({str(artist): 1})
-        else:
-            cls.artist_count[artist] +=1
-    
+    def add_to_artists_count(cls, artist): #if artist doesn't exist in artist_count, add the artist as the key and set it to 1.
+        cls.artist_count[artist] = cls.artist_count.get(artist, 0) + 1
+        return cls.artist_count
+
+
+
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
@@ -53,4 +51,3 @@ class Song:
         self.__class__.add_to_artists_count(self.artist)
 
     
-
