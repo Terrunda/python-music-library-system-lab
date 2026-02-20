f42f68120ad592753166b6492eacbf0ed539d03c
Terrunda <terrencegift49@gmail.com>
2026-02-20 22:48:13 +0300

    Developed code for the class attributes throught the use of the @classmethod decorator to access class attributes, as well as initialising the instance attributes.
    For every instance creation, each class method is called.

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,56 @@
 class Song:
-    pass
+    
+    #Class attributes
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {} #Stores a genre as a key, with the value being the number of songs for the specific genre.
+    artist_count = {} #Stores an artist as a key, with the value being the number of songs an artist has created
+
+
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count +=1
+    
+    @classmethod
+    def add_to_genres(cls, new_genre):
+        if new_genre in cls.genres:
+            return
+        else:
+            cls.genres.append(new_genre)
+    
+    @classmethod
+    def add_to_artists(cls, artist):
+        if artist in cls.artists:
+            return
+        else:
+            cls.artists.append(artist)
+    
+    @classmethod
+    def add_to_genre_count(cls, genre): #if genre doesnt exist in genre_count, add the key and set it to 1.
+        if cls.genre_count[genre] not in cls.genre_count:
+            cls.genre_count.update({str(genre): 1 })
+        else:
+            cls.genre_count[genre] +=1
+    
+    @classmethod
+    def add_to_artists_count(cls, artist):
+        if cls.artist_count[artist] not in cls.artist_count: #if artist doesn't exist in artist_count, add the artist as the key and set it to 1.
+            cls.artist_count.update({str(artist): 1})
+        else:
+            cls.artist_count[artist] +=1
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        self.__class__.add_song_to_count()
+        self.__class__.add_to_genres(self.genre)
+        self.__class__.add_to_artists(self.artist)
+        self.__class__.add_to_genre_count(self.genre)
+        self.__class__.add_to_artists_count(self.artist)
+
+    
+
